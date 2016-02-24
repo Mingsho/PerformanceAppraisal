1009ace30cece00671c084c67d7fb9ce299fbf3a
Mingsho <mlimbu@gmail.com>
2016-02-24 23:30:37 +1100

    untracked files

---
+++ PerformanceAppraisal/Scripts/Custom/PAppraisalScript.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+
+    $('#dialog-confirm').hide();
+
+    $('[id*=lnkTest]').click(function () {
+
+
+        $('#dialog-confirm').dialog({
+
+            resizable: false,
+            height: 160,
+            modal: true,
+            buttons: {
+
+                "Yes": function () {
+                    $(this).dialog("close");
+                },
+                Cancel: function () {
+                    $(this).dialog("close");
+                }
+            }
+
+        });
+
+        return false;
+
+    });
+});
